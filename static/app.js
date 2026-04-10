7814b68f3bc307073b1ec46f710e3762fbfd5648
jhulsey299-rgb <jhulsey299@gmail.com>
2026-04-10 12:08:05 -0400

    Update app.js

--- static/app.js
+++ static/app.js
@@ -2359,9 +2359,6 @@ window.onload = async function () {
     window.addEventListener("resize", applySchemaPanelWidth);
 };
 
-    window.addEventListener("resize", applySchemaPanelWidth);
-};
-
 // ======================
 // GLOBALS FOR HTML
 // ======================
