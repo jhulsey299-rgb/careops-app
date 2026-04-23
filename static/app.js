c70ccf49f37e2b57a26df6d2f12b76a76df6a2d6
jhulsey299-rgb <jhulsey299@gmail.com>
2026-04-23 08:45:01 -0400

    Update app.js

--- static/app.js
+++ static/app.js
@@ -957,12 +957,7 @@ Your answer must:
       minimumKeywordMatches: 2,
       feedbackGuide: "A strong answer includes sorting, filtering, and a clear explanation of what leadership should learn from the data.",
       executiveTakeaway: { show: false }
-    }
-
-  ]
-}
-
-        ]
+       ]
       }
     ]
   },
